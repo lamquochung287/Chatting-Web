46d8b3aa147e140c161bb07d38c0e3cc021bca74
Quoc Hung <lamquochung287@gmail.com>
2023-05-16 10:12:39 +0700

    change url server

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -5,7 +5,7 @@ import { HomePageStyled } from '../components/HomePage/style'
 import { SlideBar } from '../components/HomePage/SlideBar'
 import io from 'socket.io-client'
 
-const socket = io("http://localhost:5000")
+const socket = io("https://chatting-web-iiv3.onrender.com")
 
 export const HomePage = () => {
 
